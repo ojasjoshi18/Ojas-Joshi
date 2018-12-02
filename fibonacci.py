dfd35b10fd6e52e213effb83180053611fd71057
Ojas-Joshi <43951020+ojasjoshi18@users.noreply.github.com>
2018-12-02 15:46:29 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,14 +2,14 @@
 #Gr no:11810839
 #roll no:23
 #Division:M
-n1=0
-n2=1
+n1=0                                                         #First term in the series is always 0
+n2=1                                                         #Second term in the series is always one
 n3=0
 count=0
-times=int(input('Enter number of times you want'))
+times=int(input('Enter number of times you want'))           #Total terms in fibonacci series
 print(n2)
 while count<(times-1):
-    n3=n1+n2
+    n3=n1+n2                                                 #next number is sum of previous two numbers
     print(n3)
     n1=n2
     n2=n3
