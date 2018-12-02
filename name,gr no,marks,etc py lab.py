6be805a91aba33b57ca759fc2ee630c8dbb2a4af
Ojas-Joshi <43951020+ojasjoshi18@users.noreply.github.com>
2018-12-03 01:40:03 +0530

    Add files via upload

---
+++ name,gr no,marks,etc py lab.py
@@ -0,0 +1,17 @@
+l=[]
+while 1:
+    name=input('Enter your name: ')
+    l.append(name)
+    gr_no=int(input('Enter your GR no.: '))
+    l.append(gr_no)
+    roll_no=int(input('Enter your Roll no.: '))
+    l.append(roll_no)
+    Eng=int(input('Enter your English marks: '))
+    l.append(Eng)
+    Math=int(input('Enter your Math marks: '))
+    l.append(Math)
+    Science=int(input('Enter your Science marks: '))
+    l.append(Science)
+    break
+print(l)
+   
