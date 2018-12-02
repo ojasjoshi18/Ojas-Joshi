0b1556561e29fccb9053662a37c72457a2b9930b
Ojas-Joshi <43951020+ojasjoshi18@users.noreply.github.com>
2018-12-02 12:25:34 +0530

    Update Angstrom_number.py

--- Angstrom_number.py
+++ Angstrom_number.py
@@ -2,14 +2,14 @@
 #Gr no:11810839
 #roll no:23
 #Division:M
-def armstrong(n):
+def armstrong(n):                                #function to define armstrong number
     sum=0
     t=n
     while t>0:
-        d=t%10
-        sum=t**3
+        d=t%10                                   #Separating out the number into indiviual digits
+        sum=t**3                                 #cube of the digits is found out
         t=t//10
     return sum
-n=int(input("Enter a number"))
-y=armstrong(n)
-print(y)
+n=int(input("Enter a number"))                   #user input number is taken    
+y=armstrong(n)                                   #function is called        
+print(y)              
