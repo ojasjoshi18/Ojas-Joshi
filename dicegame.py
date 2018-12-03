b2a0970de8d38285de7e5a6a77dda24e841af374
Ojas-Joshi <43951020+ojasjoshi18@users.noreply.github.com>
2018-12-03 20:42:05 +0530

    Update dicegame.py

--- dicegame.py
+++ dicegame.py
@@ -1,15 +1,15 @@
 #Name:Ojas Joshi
 #Gr no:11810839
 #roll no:23
-#Division:M
-import random
+#Division:M 
+import random                              #Random function is imported
 from random import randint as rt
-def game(dice,faces):
+def game(dice,faces):                      #number of dice and faces are defined withon function 'Game'
     result=0
-    for roll in range(0,dice):
+    for roll in range(0,dice):             #roll is initialised taking values between 0 to dice input
         result+=rt(1,faces)
     return result
 dice=int(input())
 faces=int(input())
-result=game(dice,faces)
+result=game(dice,faces)                    #Game function is called
 print(result)
