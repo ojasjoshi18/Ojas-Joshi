e0fd402590e72f48a0188507c2540662f299ff12
Ojas-Joshi <43951020+ojasjoshi18@users.noreply.github.com>
2018-12-03 20:46:18 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -2,15 +2,15 @@
 #Gr no:11810839
 #roll no:23
 #Division:M
-num=int(input())
-total=1
-if num<0:
-     print('invalid')
-elif num==0:
-     print(factorial is 1)
+num=int(input())                #The number whose factorial is to be found is taken as input
+total=1                         
+if num<0:                       #If number is <0 then its factorial does not exist
+     print('Please enter a positive number')
+elif num==0:                    #If number is zero then its factorial is 1
+     print('factorial is 1')
 else:
- while num>0:
-        total=num*total
+ while num>0:                   #till number is greater than 0 the while loop keeps multipying it with previous number
+        total=num*total     
         num=num-1
 print("factorial is",total)
     
